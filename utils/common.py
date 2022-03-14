b88832ba318ecc618aecb0df461da0671356b882
Puncher <mcocexpert@hispeed.ch>
2022-03-14 10:16:33 +0100

    new constants file

---
+++ utils/common.py
@@ -0,0 +1,13 @@
+
+
+class Path:
+
+    json_Texts = "C:/DEV/PROJECTS/Leuchtturm/Leuchtturm-GUI/utils/json/texts.json"
+
+    png_MainIcon = "C:/DEV/PROJECTS/Leuchtturm/Leuchtturm-GUI/utils/images/lighthouse.png"
+    png_ExecutiveDark = "C:/DEV/PROJECTS/Leuchtturm/Leuchtturm-GUI/utils/images/icons/execute_dark.png"
+    png_Notebook = "C:/DEV/PROJECTS/Leuchtturm/Leuchtturm-GUI/utils/images/icons/notebook.png"
+
+    ico_MainIcon = "C:/DEV/PROJECTS/Leuchtturm/Leuchtturm-GUI/utils/images/lighthouse.ico"
+
+
