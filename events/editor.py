c5801dcb3d60aa5bcdaa2e977b83a6008b342170
Puncher <mcocexpert@hispeed.ch>
2022-03-14 17:25:42 +0100

    Removed unneeded mainWindow

--- events/editor.py
+++ events/editor.py
@@ -10,6 +10,7 @@ class EditorEvents:
 
 
     def on_btnNew_pressed(self):
+        print(self.__getattribute__("mainWindow"))
         with open(Path.json_Texts, "r") as fdata:
             data = json.load(fdata)
 
@@ -31,7 +32,7 @@ class EditorEvents:
                 fontSize=11
             )
 
-            self.mainWindow.inputText = createLineEdit(
+            self.inputText = createLineEdit(
                 1500,
                 placeholder="Enter your text",
                 fontSize=11
@@ -42,7 +43,7 @@ class EditorEvents:
                 fontSize=11
             )
 
-            self.mainWindow.inputLabel = createLineEdit(
+            self.inputLabel = createLineEdit(
                 10,
                 placeholder="Enter your label",
                 fontSize=11
@@ -64,17 +65,17 @@ class EditorEvents:
                 (descWidget, (1, 1)),
                 (emptyTextLabel, (0, 2)),
                 (inputLabelLabel, (0, 3)),
-                (self.mainWindow.inputLabel, (1, 3)),
+                (self.inputLabel, (1, 3)),
                 (inputTextLabel, (0, 4)),
-                (self.mainWindow.inputText, (1, 4)),
+                (self.inputText, (1, 4)),
                 (emptyTextLabel, (1, 5)),
                 (dlgNewTextBtnBox, (1, 6)),
             )
 
-            self.mainWindow.dlgNewText = createDialog(self.mainWindow, (500, 280), "New Text",
+            self.dlgNewText = createDialog(self.mainWindow, (500, 280), "New Text",
                                            [Qt.WindowSystemMenuHint, Qt.WindowTitleHint, Qt.WindowCloseButtonHint],
                                            layout)
-            self.mainWindow.dlgNewText.exec()
+            self.dlgNewText.exec()
 
 
     def on_btnDel_pressed(self):
@@ -91,9 +92,9 @@ class EditorEvents:
                     fontSize=11
                 )
 
-                self.mainWindow.delDropdownWidget = createComboBox(data.keys())
+                self.delDropdownWidget = createComboBox(data.keys())
 
-                self.mainWindow.dlgDelTextBtnBox = createDialogButtonBox(
+                self.dlgDelTextBtnBox = createDialogButtonBox(
                     [
                         ("Delete && Save", QDialogButtonBox.AcceptRole),
                         ("Cancel", QDialogButtonBox.RejectRole)
@@ -105,14 +106,14 @@ class EditorEvents:
                 layout = createGridLayout(
                     (titleWidget, (0, 0)),
                     (descWidget, (0, 1)),
-                    (self.mainWindow.delDropdownWidget, (0, 2)),
-                    (self.mainWindow.dlgDelTextBtnBox, (1, 3))
+                    (self.delDropdownWidget, (0, 2)),
+                    (self.dlgDelTextBtnBox, (1, 3))
                 )
 
-                self.mainWindow.dlgDelText = createDialog(self.mainWindow, (500, 180), "Delete Text",
+                self.dlgDelText = createDialog(self.mainWindow, (500, 180), "Delete Text",
                                                [Qt.WindowSystemMenuHint, Qt.WindowTitleHint, Qt.WindowCloseButtonHint],
                                                layout)
-                self.mainWindow.dlgDelText.exec()
+                self.dlgDelText.exec()
 
 
     def on_btnClear_pressed(self):
@@ -130,8 +131,8 @@ class EditorEvents:
     def on_btnsNewText_pressed(self, btn):
         if btn == QDialogButtonBox.Save:
 
-            label = self.mainWindow.inputLabel.text()
-            text = self.mainWindow.inputText.text()
+            label = self.inputLabel.text()
+            text = self.inputText.text()
 
             if label == "" and text == "":
                 createMessageBox(self.mainWindow, "Error!", "Text and Label is empty, please try again",
@@ -156,7 +157,7 @@ class EditorEvents:
                             json.dump(data, fdata, sort_keys=True, indent=4)
 
                         updateEditorTable(self.mainWindow.tableWidget)
-                        self.mainWindow.dlgNewText.close()
+                        self.dlgNewText.close()
                         createMessageBox(self.mainWindow, "New Text", "Your text has been created!", [QMessageBox.Ok],
                                          QMessageBox.Information)
 
@@ -169,7 +170,7 @@ class EditorEvents:
 
 
         elif btn == QDialogButtonBox.Cancel:
-            self.mainWindow.dlgNewText.close()
+            self.dlgNewText.close()
 
         else:
             raise TypeError(f"Unexpected argument '{btn}'.")
@@ -177,7 +178,7 @@ class EditorEvents:
 
     def on_btnsDelText_pressed(self, btn):
         if btn == QDialogButtonBox.Save:
-            selectedLabel = self.mainWindow.delDropdownWidget.currentText()
+            selectedLabel = self.delDropdownWidget.currentText()
 
             with open(Path.json_Texts, "r") as fdata:
                 data = json.load(fdata)
@@ -186,12 +187,12 @@ class EditorEvents:
                     json.dump(data, fdata, sort_keys=True, indent=4)
 
                 updateEditorTable(self.mainWindow.tableWidget)
-                self.mainWindow.dlgDelText.close()
+                self.dlgDelText.close()
                 createMessageBox(self.mainWindow, "Delete Text", "Your text has been deleted!", [QMessageBox.Ok],
                                  QMessageBox.Information)
 
         elif btn == QDialogButtonBox.Cancel:
-            self.mainWindow.dlgDelText.close()
+            self.dlgDelText.close()
 
 
     def on_btnsClearText_pressed(self, btnName):
